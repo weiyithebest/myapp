2b9a3b7a12b58dbc0e063b6436753993ac620b0f
Weiyi Wei <weiweiyi670@gmail.com>
2024-10-07 13:45:35 +0900

    Update home and about page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
 class HomeController < ApplicationController
+    def myhome
+    end
+
+    def about
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root 'home#index'
+  root 'home#myhome'
+  get 'about', to: 'home#about'
 end
