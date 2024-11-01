5a37c86c78466156e2ac0146776dfd97a6ef8b35
Weiyi Wei <weiweiyi670@gmail.com>
2024-11-01 16:40:56 +0900

    Revise destroy problem

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -47,7 +47,6 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-
     redirect_to root_path, status: :see_other
   end
 
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
-pin "@rails/ujs", to: "rails-ujs.js"
 # pin "@rails/ujs", to: "rails-ujs.js"
+# # pin "@rails/ujs", to: "rails-ujs.js"
